057587acc6732fbdc0ccebb69bff0642c5f81150
Jacob Foshee <jacobf@gmail.com>
2024-08-27 10:28:49 -0500

    feat(gl): clear a GL canvas!

--- Program.cs
+++ Program.cs
@@ -3,6 +3,10 @@ using System.Runtime.InteropServices.JavaScript;
 
 Console.WriteLine("Hello, Browser!");
 
+// Cornflower blue
+MyClass.ClearColor(0.39f, 0.58f, 0.93f, 1);
+MyClass.Clear(MyClass.COLOR_BUFFER_BIT);
+
 public partial class MyClass
 {
     [JSExport]
@@ -15,4 +19,12 @@ public partial class MyClass
 
     [JSImport("window.location.href", "main.js")]
     internal static partial string GetHRef();
+
+    [JSImport("gl.clearColor", "main.js")]
+    internal static partial void ClearColor(float red, float green, float blue, float alpha);
+
+    public const int COLOR_BUFFER_BIT = 0x00004000;
+
+    [JSImport("gl.clear", "main.js")]
+    internal static partial void Clear(int mask);
 }
--- wwwroot/main.js
+++ wwwroot/main.js
@@ -1,19 +1,21 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
+import { dotnet } from "./_framework/dotnet.js";
 
-import { dotnet } from './_framework/dotnet.js'
+// Get WebGL context from the canvas
+const canvas = document.getElementById("glCanvas");
+const gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
 
 const { setModuleImports, getAssemblyExports, getConfig } = await dotnet
-    .withDiagnosticTracing(false)
-    .withApplicationArgumentsFromQuery()
-    .create();
+  .withDiagnosticTracing(false)
+  .withApplicationArgumentsFromQuery()
+  .create();
 
-setModuleImports('main.js', {
-    window: {
-        location: {
-            href: () => globalThis.window.location.href
-        }
-    }
+setModuleImports("main.js", {
+  gl: gl,
+  window: {
+    location: {
+      href: () => globalThis.window.location.href,
+    },
+  },
 });
 
 const config = getConfig();
@@ -21,5 +23,5 @@ const exports = await getAssemblyExports(config.mainAssemblyName);
 const text = exports.MyClass.Greeting();
 console.log(text);
 
-document.getElementById('out').innerHTML = text;
+document.getElementById("out").innerHTML = text;
 await dotnet.run();
